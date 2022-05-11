99819ccf21ee61cd11307b9ed3678866966f3140
Linson45 <61908017+Linson45@users.noreply.github.com>
2022-05-11 17:00:28 +0530

    fonts 2

---
+++ src/assets/monserrat-fonts/fonts.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+import Montserrat from "./MontserratAlternates-Regular.ttf";
+import MontserratBold from "./MontserratAlternates-Bold.ttf";
+import MontserratMedium from "./MontserratAlternates-Medium.ttf";
+import MontserratSemiBold from "./MontserratAlternates-SemiBold.ttf";
+
+export default createGlobalStyle`
+     * {
+        font-family: 'Roboto', sans-serif;
+
+ 
+  }
+   
+`;
