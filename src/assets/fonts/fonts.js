885ca1061ce6203720b000c45c1803e791215cc9
Linson45 <61908017+Linson45@users.noreply.github.com>
2022-05-11 16:58:45 +0530

    fonts

---
+++ src/assets/fonts/fonts.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+import Montserrat from "./montserrat-regular.woff";
+import Montserrat2 from "./montserrat-regular.woff2";
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'Roboto';
+        font-weight: 300;
+        font-style: normal;
+    }
+`;
